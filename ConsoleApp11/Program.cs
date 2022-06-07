25ddf7825f1c07302b9159026c33963498a87b3d
saydi <saydi@ENGINEER>
2022-06-07 18:30:45 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество элементов ");
+        int a = int.Parse(Console.ReadLine());
+        int[] b = new int[a];
+        Console.WriteLine("Заполните массив ");
+        for (int i = 0; i < a; i++)
+        {
+            b[i] = int.Parse(Console.ReadLine());
+        }
+        Console.WriteLine("Порядковый номер числа N ");
+        int c = int.Parse(Console.ReadLine());
+        Console.WriteLine("Число в массиве: " + b[c - 1]);
+    }
+}
